65896dd7de2bfacfb6c12650b2c5453c3ce6ae3b
Denis <DenisArtisanDeveloppeur@gmail.com>
2024-01-09 17:09:41 +0100

    correction grid
    DGcol DGrow boostrap

--- designSystem/src/Pages/Home.jsx
+++ designSystem/src/Pages/Home.jsx
@@ -2,16 +2,16 @@ import { Alert } from "../Components/Feedback/Alert";
 
 export function Home() {
   return <div className="grid">
-    <div className="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+    <div className="DGcol-xxl-6 DGcol-xl-6 DGcol-lg-6 DGcol-md-8 DGcol-sm-12 DGcol-xs-12">
       <div className="subtitle">Typographie custom</div>
-      <h1>Title 1</h1>
-      <h2>Title 2</h2>
-      <h3>Title 3</h3>
-      <h4>Title 4</h4>
-      <h5>Title 5</h5>
-      <h6>Title 6</h6>
+      <h1>Title heading 1</h1>
+      <h2>Subtitle heading 2</h2>
+      <h3>Title level  3</h3>
+      <h4>Title level 4</h4>
+      <h5>Title level 5</h5>
+      <h6>Title last 6</h6>
     </div>
-    <div className="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+    <div className="DGcol-xxl-6 DGcol-xl-6 DGcol-lg-6 DGcol-md-4 DGcol-sm-12 DGcol-xs-12">
       <div className="subtitle">Alert Boostrap</div>
       <Alert>
         A simple primary alert—check it out!
@@ -26,5 +26,6 @@ export function Home() {
         A simple warning alert—check it out!
       </Alert>
     </div>
+
   </div>
 }
