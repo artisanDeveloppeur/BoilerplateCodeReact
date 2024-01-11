207ee835ced98bbf2cee9009428071450e3afaa4
Denis <DenisArtisanDeveloppeur@gmail.com>
2024-01-11 13:39:54 +0100

    card css

--- designSystem/src/Pages/Home.jsx
+++ designSystem/src/Pages/Home.jsx
@@ -31,6 +31,88 @@ const slidesCarousel = [
 
 export function Home() {
   return <div className="grid">
+    <div>
+      <div className="wrapper__cards">
+        <section className="card">
+          <picture>
+            <img className="" src="https://picsum.photos/seed/img1/300/200" alt="" />
+          </picture>
+          <h2 className="card__title">Titre de l'article</h2>
+          <p className="card__description js-read-more is-expanded">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <span className="read-more__link-wrap">
+            <a id="read-more_0" className="read-more__link rounded" >
+              <span className="dashicons ">+ </span>
+              Continuer à lire
+            </a>
+          </span>
+        </section>
+        <section className="card">
+          <picture>
+            <img className="" src="https://picsum.photos/seed/img2/300/200" alt="" />
+          </picture>
+          <h2 className="card__title">Titre de l'article</h2>
+          <p className="card__description js-read-more is-expanded">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <span className="read-more__link-wrap">
+            <a id="read-more_0" className="read-more__link rounded" >
+              <span className="dashicons dashicons-minus">+ </span>
+              Continuer à lire
+            </a>
+          </span>
+        </section>
+        <section className="card">
+          <picture>
+            <img className="" src="https://picsum.photos/seed/img3/300/200" alt="" />
+          </picture>
+          <h2 className="card__title">Titre de l'article</h2>
+          <p className="card__description js-read-more is-expanded"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+          <span className="read-more__link-wrap">
+            <a id="read-more_0" className="read-more__link rounded" >
+              <span className="dashicons dashicons-minus">+ </span>
+              Voir moins de texte
+            </a>
+          </span>
+        </section>
+        <section className="card">
+          <picture>
+            <img className="" src="https://picsum.photos/seed/img4/300/200" alt="" />
+          </picture>
+          <h2 className="card__title">Titre de l'article</h2>
+          <p className="card__description js-read-more is-expanded">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <span className="read-more__link-wrap">
+            <a id="read-more_0" className="read-more__link rounded" >
+              <span className="dashicons ">+ </span>
+              Continuer à lire
+            </a>
+          </span>
+        </section>
+        <section className="card">
+          <picture>
+            <img className="" src="https://picsum.photos/seed/img5/300/200" alt="" />
+          </picture>
+          <h2 className="card__title">Titre de l'article</h2>
+          <p className="card__description js-read-more is-expanded">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <span className="read-more__link-wrap">
+            <a id="read-more_0" className="read-more__link rounded" >
+              <span className="dashicons dashicons-minus">+ </span>
+              Continuer à lire
+            </a>
+          </span>
+        </section>
+        <section className="card">
+          <picture>
+            <img className="" src="https://picsum.photos/seed/img6/300/200" alt="" />
+          </picture>
+          <h2 className="card__title">Titre de l'article</h2>
+          <p className="card__description js-read-more is-expanded"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+          <span className="read-more__link-wrap">
+            <a id="read-more_0" className="read-more__link rounded" >
+              <span className="dashicons dashicons-minus">+ </span>
+              Voir moins de texte
+            </a>
+          </span>
+        </section>
+      </div>
+    </div>
     <div className="gcol-xxl-12 gcol-xl-12 gcol-lg-12 gcol-md-12 gcol-sm-12 gcol-xs-12">
       <div className="subtitle">Carousel with indicators</div>
       <Carousel data={slidesCarousel} />
