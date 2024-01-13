054d550692def1921811fa13066ea030fae3fcf3
Denis <DenisArtisanDeveloppeur@gmail.com>
2024-01-13 10:08:25 +0100

    pressKey

--- designSystem/src/Components/ContentDisplay/Carousel.jsx
+++ designSystem/src/Components/ContentDisplay/Carousel.jsx
@@ -1,16 +1,57 @@
 import React, { useState } from "react";
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+import { useEffect } from 'react';
 
 export const Carousel = ({ data }) => {
   const [slideCarousel, setSlideCarousel] = useState(0);
 
   const nextSlideCarousel = () => {
     setSlideCarousel(slideCarousel === data.length - 1 ? 0 : slideCarousel + 1);
+
   };
 
   const prevSlideCarousel = () => {
     setSlideCarousel(slideCarousel === 0 ? data.length - 1 : slideCarousel - 1);
+
   };
+
+
+
+  const pressKey = () => {
+    // your logic here
+    console.log('pressed Enter');
+  };
+
+  useEffect(() => {
+    const keyDownHandler = event => {
+      console.log('User pressed key: ', event.key)
+      console.log('User pressed keyCode: ', event.keyCode)
+      if (event.key) {
+        event.preventDefault();
+
+        // 👇️ your logic here
+        pressKey();
+      }
+
+      if (event.keyCode == '37') {
+        // left arrow
+
+      }
+
+      if (event.keyCode == '39') {
+        // right arrow
+
+      }
+
+
+    };
+
+    document.addEventListener('keydown', keyDownHandler);
+
+    return () => {
+      document.removeEventListener('keydown', keyDownHandler);
+    };
+  }, []);
 
   return (
     <>
